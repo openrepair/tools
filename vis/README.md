5a6e8f574e510873cba1ce8e6047108e6d0712c3
zenlan <mszpak@zenlan.com>
2023-06-26 13:16:33 +0100

    Adds vis folder with coffee/tea example

---
+++ vis/README.md
@@ -0,0 +1,13 @@
+# Data visualisation
+
+An example using SQL, Python, JSON, ChartJS and HTML.
+
+![Coffee v Tea](ords_coffee_v_tea.png)
+
+## Javascript
+
+[ChartJS](https://www.chartjs.org/)
+
+[Data-Driven Documents (D3)](https://d3js.org/)
+
+[Plotly](https://plotly.com/javascript/)
---
+++ vis/ords_coffee_v_tea.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import mysql.connector
+import os
+import json
+import pandas as pd
+from dotenv import load_dotenv
+load_dotenv()
+
+# Standalong script to dump a JSON dataset for use in data visualisation.
+
+def query_fetchall(sql):
+    result = False
+    try:
+        dbh = mysql.connector.connect(
+            host=os.environ['ORDS_DB_HOST'],
+            database=os.environ['ORDS_DB_DATABASE'],
+            user=os.environ['ORDS_DB_USER'],
+            password=os.environ['ORDS_DB_PWD']
+        )
+        cursor = dbh.cursor(dictionary=True)
+        cursor.execute(sql)
+        result = cursor.fetchall()
+    except mysql.connector.Error as error:
+        print("MySQL exception: {}".format(error))
+
+    finally:
+        if dbh.is_connected():
+            cursor.close()
+            dbh.close()
+        return result
+
+
+sql = """
+    SELECT
+    country,
+    product_category as product,
+    COUNT(*) as records
+    FROM `{}`
+    WHERE product_category IN ('Kettle', 'Coffee Maker')
+    GROUP BY product_category, country
+    HAVING records > 50
+    ORDER BY country, product_category, records DESC;
+    """
+dfsub = pd.DataFrame(query_fetchall(sql.format(os.environ['ORDS_DATA'])))
+with open('ords_coffee_v_tea.json', 'w') as f:
+    json.dump(dfsub.set_index('country').to_dict(
+        'records'), f, indent=4, ensure_ascii=False)
