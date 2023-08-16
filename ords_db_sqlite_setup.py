a5cc757448761b1bcec3d72b9a300f86462c90bb
zenlan <mszpak@zenlan.com>
2023-08-16 08:21:58 +0100

    Refactors sqlite setup script

--- ords_db_sqlite_setup.py
+++ ords_db_sqlite_setup.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
-import pandas as pd
 from funcs import *
+import pandas as pd
 import sqlite3
-from sqlite3 import Error
 
 logger = logfuncs.init_logger(__file__)
 
@@ -20,19 +19,29 @@ def sqlite_connect():
     try:
         con = sqlite3.connect('ords_test')
         con.row_factory = dict_factory
-    except Exception as error:
+    except sqlite3.Error as error:
         print("Exception: {}".format(error))
     finally:
         return con
 
 
-def show_tables():
+def log_tables():
     try:
+        table_cats = envfuncs.get_var('ORDS_CATS')
+        table_data = envfuncs.get_var('ORDS_DATA')
         con = sqlite_connect()
-        for row in con.execute("SELECT name FROM sqlite_master WHERE type='table'"):
-            print(row)
-            logger.debug(row)
-    except Exception as error:
+        sql_tbl = """SELECT name, sql FROM sqlite_master WHERE type='table' AND name='{}' ORDER BY name"""
+        sql_idx = """SELECT name, tbl_name, sql FROM sqlite_master WHERE type='index' AND tbl_name='{}' ORDER BY tbl_name"""
+        for row in con.execute(sql_tbl.format(table_cats)):
+            logger.debug(row['sql'])
+        for row in con.execute(sql_idx.format(table_cats)):
+            logger.debug(row['sql'])
+        for row in con.execute(sql_tbl.format(table_data)):
+            logger.debug(row['sql'])
+        for row in con.execute(sql_idx.format(table_data)):
+            logger.debug(row['sql'])
+        print('See logfile for table structures')
+    except sqlite3.Error as error:
         print("Exception: {}".format(error))
     finally:
         if (con):
@@ -42,101 +51,89 @@ def show_tables():
 def drop_tables():
     try:
         con = sqlite_connect()
-        table_cats = envfuncs.get_var('ORDS_CATS')
-        con.execute("DROP TABLE IF EXISTS `{}`".format(table_cats))
-        table_data = envfuncs.get_var('ORDS_DATA')
-        con.execute("DROP TABLE IF EXISTS `{}`".format(table_data))
+        con.execute("DROP TABLE IF EXISTS `{}`".format(
+            envfuncs.get_var('ORDS_CATS')))
+        con.execute("DROP TABLE IF EXISTS `{}`".format(
+            envfuncs.get_var('ORDS_DATA')))
         con.commit()
-        show_tables()
-    except Exception as error:
+    except sqlite3.Error as error:
         print("Exception: {}".format(error))
     finally:
         if (con):
             con.close()
 
 
-def put_table_categories():
-    try:
-        table_cats = envfuncs.get_var('ORDS_CATS')
-        con = sqlite_connect()
-        con.execute("CREATE TABLE `{}`(product_category_id integer primary key, product_category varchar (64))".format(table_cats))
-        con.execute("CREATE INDEX `product_category` ON `{}` (`product_category`)".format(table_cats))
-        con.commit()
-        path = pathfuncs.get_path([pathfuncs.ORDS_DIR, table_cats + '.csv'])
-        logger.debug('Reading file "{}"'.format(path))
-        dfcats = pd.read_csv(path)
-        vals = list(zip(*[dfcats[col] for col in dfcats]))
-        sql = "INSERT INTO `{}`(product_category_id, product_category) VALUES(?, ?)".format(table_cats)
-        logger.debug(sql)
-        with con:
-            con.executemany(sql, vals)
-        rows = con.total_changes
-        logger.debug('{} rows written to table "{}"'.format(rows, table_cats))
-        for row in con.execute("SELECT * FROM `{}`".format(table_cats)):
-            print(row)
-
-    except Exception as error:
-        print("Exception: {}".format(error))
-    finally:
-        show_tables()
-        if (con):
-            con.close()
+def put_table_categories(schemas):
+    table = envfuncs.get_var('ORDS_CATS')
+    indices = ['product_category']
+    put_table(table, schemas[table]['sql'], indices, 'cat')
 
 
-def put_table_data():
+def put_table_data(schemas):
     table_data = envfuncs.get_var('ORDS_DATA')
+    indices = ['product_category',
+               'product_category_id',
+               'data_provider',
+               'product_age',
+               'repair_status',
+               'repair_barrier_if_end_of_life',
+               'event_date']
+    put_table(table_data, schemas[table_data]['sql'], indices, 'dat')
+
+
+def put_table(table, sql, indices, prefix):
     try:
         con = sqlite_connect()
-        sql = """
-        CREATE TABLE `{}` (
-        `id` varchar(32) PRIMARY KEY,
-        `data_provider` varchar(32),
-        `country` varchar(3),
-        `partner_product_category` varchar(255),
-        `product_category` varchar(64),
-        `product_category_id` integer,
-        `brand` varchar(128),
-        `year_of_manufacture` varchar(4),
-        `product_age` varchar(6),
-        `repair_status` varchar(16),
-        `repair_barrier_if_end_of_life` varchar(32),
-        `group_identifier` varchar(64),
-        `event_date` varchar(10),
-        `problem` text(255)
-        )
-        """.format(table_data)
         con.execute(sql)
-        con.execute("CREATE INDEX `product_category_1` ON `{}` (`product_category`)".format(table_data))
-        con.execute("CREATE INDEX `product_category_id_1` ON `{}` (`product_category_id`)".format(table_data))
-        con.execute("CREATE INDEX `data_provider` ON `{}` (`data_provider`)".format(table_data))
-        con.execute("CREATE INDEX `product_age` ON `{}` (`product_age`)".format(table_data))
-        con.execute("CREATE INDEX `repair_status` ON `{}` (`repair_status`)".format(table_data))
-        con.execute("CREATE INDEX `repair_barrier_if_end_of_life` ON `{}` (`repair_barrier_if_end_of_life`)".format(table_data))
-        con.execute("CREATE INDEX `event_date`ON `{}` (`event_date`)".format(table_data))
-        con.execute("CREATE INDEX `problem`ON `{}` (`problem`)".format(table_data))
+        for idx in indices:
+            con.execute(
+                "CREATE INDEX `{2}_{1}` ON `{0}` (`{1}`)".format(table, idx, prefix))
         con.commit()
-        path = pathfuncs.get_path([pathfuncs.ORDS_DIR, table_data + '.csv'])
+        path = pathfuncs.get_path([pathfuncs.ORDS_DIR, table + '.csv'])
         logger.debug('Reading file "{}"'.format(path))
-        dfdata = pd.read_csv(path)
-        vals = list(zip(*[dfdata[col] for col in dfdata]))
-        sql = "INSERT INTO `{}`({}) VALUES({})".format(table_data, ", ".join(dfdata.columns), ",".join(["?"] * len(dfdata.columns)))
+        df = pd.read_csv(path)
+        vals = list(zip(*[df[col] for col in df]))
+        sql = "INSERT INTO `{}`({}) VALUES({})".format(table, ", ".join(
+            df.columns), ",".join(["?"] * len(df.columns)))
         logger.debug(sql)
         with con:
             con.executemany(sql, vals)
         rows = con.total_changes
-        logger.debug('{} rows written to table "{}"'.format(rows, table_data))
-        for row in con.execute("SELECT * FROM `{}` LIMIT 1".format(table_data)):
+        logger.debug('{} rows written to table "{}"'.format(rows, table))
+        for row in con.execute("SELECT * FROM `{}` LIMIT 1".format(table)):
             print(row)
-            break
-
-    except Exception as error:
-        print("Exception: {}".format(error))
+    except sqlite3.Error as error:
+        print("Exception: {}".format(table))
+        print(error)
     finally:
-        show_tables()
         if (con):
             con.close()
 
 
+def get_schemas():
+    schemas = dbfuncs.table_schemas()
+    for name, stru in schemas.items():
+        sql = """ CREATE TABLE `{}` (""".format(name)
+        for field in stru['fields']:
+            if field['type'] == 'string':
+                if field['name'] != 'problem':
+                    sql += "\n`{}` varchar(255),".format(field['name'])
+                elif field['name'] == 'problem':
+                    sql += "\n`{}` text(255),".format(field['name'])
+            elif field['type'] == 'integer':
+                sql += "\n`{}` integer,".format(field['name'])
+            elif field['type'] == 'number':
+                sql += "\n`{}` real,".format(field['name'])
+            elif field['type'] == 'date':
+                sql += "\n`{}` varchar(10),".format(field['name'])
+        sql += """\nPRIMARY KEY (`{}`) );""".format(stru['pkey'])
+        schemas[name]['sql'] = sql
+    return schemas
+
+
+schemas = get_schemas()
 drop_tables()
-put_table_categories()
-put_table_data()
+log_tables()
+put_table_categories(schemas)
+put_table_data(schemas)
+log_tables()
