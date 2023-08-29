068da72f2a5bd635add65e9fe56f42d0a1073f28
zenlan <mszpak@zenlan.com>
2023-08-30 00:43:16 +0100

    Refactors device regexes script, adds "all" test

--- ords_regex_devices.py
+++ ords_regex_devices.py
@@ -7,9 +7,6 @@ import re
 logger = logfuncs.init_logger(__file__)
 
 
-# Given a list of "terms" (actually mini-regexes), return a whole regular expression string for each key.
-
-
 def build_regexes():
 
     rxelems = pd.read_csv(pathfuncs.DATA_DIR +
@@ -36,7 +33,7 @@ def precompile_regexes(regexes):
     return regexes
 
 
-def slice_item_types():
+def slice_item_typesX():
 
     df = pd.read_csv(pathfuncs.path_to_ords_csv(), dtype=str,
                      keep_default_na=False, na_values="")
@@ -51,11 +48,24 @@ def slice_item_types():
     return df
 
 
-def get_test_data(sample=0.25, categories=[]):
+def get_item_types(columns):
 
-    testterms = slice_item_types()
-    testterms['has-match'] = False
-    testterms['matched'] = ''
+    df = pd.read_csv(pathfuncs.path_to_ords_csv(), dtype=str,
+                     keep_default_na=False, na_values="")
+    df.rename(
+        columns={'partner_product_category': 'item_type'}, inplace=True)
+    df.item_type = df.item_type.apply(
+        lambda s: s.split('~').pop().strip())
+    df = df.reindex(columns=columns)
+    df = df.groupby(columns).size().reset_index(name='records').sort_values(
+        by=columns, key=lambda col: col.str.lower())
+    df['matched'] = ''
+    return df
+
+
+def get_test_data(sample=0.25, columns=[], categories=[]):
+
+    testterms = get_item_types(columns)
     if len(categories) > 0:
         testterms = testterms.loc[testterms.product_category.isin(categories)]
     if sample < 1:
@@ -63,7 +73,6 @@ def get_test_data(sample=0.25, categories=[]):
     return testterms
 
 
-# test each test term for a single item_type.
 def test_one(testterms, key, regexes):
     logger.debug('*** {} ***'.format(key))
     rx = regexes.at[key, 'rx']
@@ -85,75 +94,104 @@ def test_one(testterms, key, regexes):
     return testterms
 
 
+# Test terms from a set of categories agains a regex.
+def test_pairs(regexes):
+    pairs = [
+        ('iron', ['Iron']),
+        ('coffee-machine', ['Coffee maker']),
+        ('kettle', ['Kettle']),
+        ('microwave', ['Large home electrical']),
+        ('lawnmower', ['Large home electrical']),
+        ('sport-exercise', ['Large home electrical']),
+        ('garden-machine', ['Large home electrical']),
+        ('sewing-machine', ['Sewing machine']),
+        ('portable-audio-video', ['Handheld entertainment device']),
+        ('gaming-console', ['Handheld entertainment device']),
+        ('gaming-portable', ['Handheld entertainment device']),
+        ('speaker', ['Handheld entertainment device', 'TV and gaming-related accessories',
+                     'PC accessory', 'Hi-Fi separates', 'Hi-Fi integrated']),
+        ('remote-control', ['Handheld entertainment device',
+                            'PC accessory', 'TV and gaming-related accessories']),
+        ('dishwasher', ['Large home electrical']),
+        ('washing-machine', ['Large home electrical']),
+        ('clothes-dryer', ['Large home electrical']),
+        ('fridge', ['Large home electrical']),
+        ('freezer', ['Large home electrical']),
+        ('air-conditioner', ['Aircon/dehumidifier']),
+        ('vacuum-cleaner', ['Vacuum']),
+        ('computer-tablet', ['Tablet']),
+        ('computer-desktop',  ['Desktop computer']),
+        ('computer-laptop', ['Laptop']),
+        ('computer-accessory', ['PC accessory']),
+        ('computer-auto', ['Tablet', 'Handheld entertainment device',
+                           'PC accessory', 'TV and gaming-related accessories']),
+        ('printer', ['Printer/scanner']),
+        ('telecom', ['Small home electrical', 'Misc']),
+        ('telecom-mobile', ['Mobile']),
+        ('headphone', ['Headphones']),
+        ('set-top-box', ['TV and gaming-related accessories']),
+        ('camera', ['Digital compact camera', 'DSLR/video camera']),
+        ('flatpanel-screen', ['Flat screen']),
+        ('lighting', ['Lamp', 'Decorative or safety lights']),
+        ('power-tool', ['Power tool']),
+        ('garden-machine', ['Power tool', 'Large home electrical']),
+        ('shredder', ['Paper shredder']),
+        ('projector', ['Projector']),
+        ('toaster', ['Toaster']),
+        ('watch-clock', ['Watch/clock']),
+        ('hairdryer', ['Hair dryer']),
+        ('food-processor', ['Food processor']),
+        ('charger-adapter', ['Battery/charger/adapter']),
+        ('musical-instrument', ['Musical instrument']),
+        ('small-kitchen', ['Small kitchen item']),
+        ('small-domestic', ['Small home electrical']),
+        ('personal-medical', ['Hair & beauty item',
+         'Small home electrical', 'Misc']),
+        ('personal-grooming', ['Hair & beauty item']),
+        ('personal-hygiene', ['Hair & beauty item']),
+        ('heater', ['Small home electrical', 'Misc']),
+        ('toy', ['Toy']),
+    ]
+    dftest = pd.DataFrame()
+    for i in range(0, len(pairs)):
+        data = get_test_data(sample=1, columns=[
+                             'product_category', 'item_type'], categories=pairs[i][1])
+        data['has-match'] = False
+        test = test_one(data, pairs[i][0], regexes)
+        dftest = pd.concat([dftest, test])
+
+    testmatches = dftest.loc[dftest['has-match'] == True].sort_values(
+        by=['product_category', 'matched'], ascending=[True, True])
+    testmatches.to_csv(pathfuncs.OUT_DIR +
+                       '/devices_regex_pairs_matched.csv', index=False)
+    testmisses = dftest.loc[dftest['has-match'] == False].sort_values(
+        by=['product_category', 'matched'], ascending=[True, True])
+    testmisses.to_csv(pathfuncs.OUT_DIR +
+                      '/devices_regex_pairs_missed.csv', index=False)
+
+
+# Test each term against each regex.
+def test_all(regexes):
+    testterms = get_test_data(sample=1, columns=['item_type'])
+    for n, term in testterms.iterrows():
+        print("{}".format(term['item_type']))
+        matched = []
+        for key, regex in regexes.iterrows():
+            matches = regex.rx.search(term['item_type'])
+            if matches != None:
+                matched.append("({}:{})".format(key, matches.group()))
+        testterms.at[n, 'matched'] = "|".join(matched)
+
+    testmatches = testterms.loc[testterms['matched'] > ''].sort_values(
+        by=['item_type', 'matched'], ascending=[True, True])
+    testmatches.to_csv(pathfuncs.OUT_DIR +
+                       '/devices_regex_all_matched.csv', index=False)
+    testmisses = testterms.loc[testterms['matched'] == ''].sort_values(
+        by=['item_type', 'matched'], ascending=[True, True])
+    testmisses.to_csv(pathfuncs.OUT_DIR +
+                      '/devices_regex_all_missed.csv', index=False)
+
+
 regexes = precompile_regexes(build_regexes())
-
-pairs = [
-    ('iron', ['Iron']),
-    ('coffee-machine', ['Coffee maker']),
-    ('kettle', ['Kettle']),
-    ('microwave', ['Large home electrical']),
-    ('lawnmower', ['Large home electrical']),
-    ('sport-exercise', ['Large home electrical']),
-    ('garden-machine', ['Large home electrical']),
-    ('sewing-machine', ['Sewing machine']),
-    ('portable-audio-video', ['Handheld entertainment device']),
-    ('gaming-console', ['Handheld entertainment device']),
-    ('gaming-portable', ['Handheld entertainment device']),
-    ('speaker', ['Handheld entertainment device', 'TV and gaming-related accessories',
-     'PC accessory', 'Hi-Fi separates', 'Hi-Fi integrated']),
-    ('remote-control', ['Handheld entertainment device',
-     'PC accessory', 'TV and gaming-related accessories']),
-    ('dishwasher', ['Large home electrical']),
-    ('washing-machine', ['Large home electrical']),
-    ('clothes-dryer', ['Large home electrical']),
-    ('fridge', ['Large home electrical']),
-    ('freezer', ['Large home electrical']),
-    ('air-conditioner', ['Aircon/dehumidifier']),
-    ('vacuum-cleaner', ['Vacuum']),
-    ('computer-tablet', ['Tablet']),
-    ('computer-desktop',  ['Desktop computer']),
-    ('computer-laptop', ['Laptop']),
-    ('computer-accessory', ['PC accessory']),
-    ('computer-auto', ['Tablet', 'Handheld entertainment device',
-     'PC accessory', 'TV and gaming-related accessories']),
-    ('printer', ['Printer/scanner']),
-    ('telecom', ['Small home electrical', 'Misc']),
-    ('telecom-mobile', ['Mobile']),
-    ('headphone', ['Headphones']),
-    ('set-top-box', ['TV and gaming-related accessories']),
-    ('camera', ['Digital compact camera', 'DSLR/video camera']),
-    ('flatpanel-screen', ['Flat screen']),
-    ('lighting', ['Lamp', 'Decorative or safety lights']),
-    ('power-tool', ['Power tool']),
-    ('garden-machine', ['Power tool', 'Large home electrical']),
-    ('shredder', ['Paper shredder']),
-    ('projector', ['Projector']),
-    ('toaster', ['Toaster']),
-    ('watch-clock', ['Watch/clock']),
-    ('hairdryer', ['Hair dryer']),
-    ('food-processor', ['Food processor']),
-    ('charger-adapter', ['Battery/charger/adapter']),
-    ('musical-instrument',['Musical instrument']),
-    ('small-kitchen',['Small kitchen item']),
-    ('small-domestic',['Small home electrical']),
-    ('personal-medical', ['Hair & beauty item', 'Small home electrical', 'Misc']),
-    ('personal-grooming',['Hair & beauty item']),
-    ('personal-hygiene',['Hair & beauty item']),
-    ('heater',['Small home electrical', 'Misc']),
-    ('toy',['Toy']),
-]
-
-
-dftest = pd.DataFrame()
-for i in range(0, len(pairs)):
-    data = get_test_data(sample=1, categories=pairs[i][1])
-    test = test_one(data, pairs[i][0], regexes)
-    dftest = pd.concat([dftest, test])
-
-
-testmatches = dftest.loc[dftest['has-match'] == True].sort_values(by=['product_category', 'matched'], ascending=[True, True])
-testmatches.to_csv(pathfuncs.OUT_DIR +
-                   '/devices_regex_matched.csv', index=False)
-testmisses = dftest.loc[dftest['has-match'] == False].sort_values(by=['product_category', 'matched'], ascending=[True, True])
-testmisses.to_csv(pathfuncs.OUT_DIR +
-                  '/devices_regex_missed.csv', index=False)
+test_pairs(regexes)
+test_all(regexes)
