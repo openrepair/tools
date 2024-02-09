fc90aa29f5e60af1bf95d3100c50f78c1e6a73ae
zenlan <mszpak@zenlan.com>
2024-02-09 07:49:23 +0000

    Makes deepl and poetry scripts interactive

--- ords_poetry.py
+++ ords_poetry.py
@@ -6,18 +6,28 @@ import os
 import json
 from nltk import tokenize
 
-logger = logfuncs.init_logger(__file__)
-
 # Challenge - build a Repair Haiku generator.
 # https://spacy.io/universe/project/spacy_syllables
 
 
-def gen(lang="en", lines=5, verses=12):
+def write_poem(lang="en", lines=5, verses=12):
     df = pd.read_csv(pathfuncs.DATA_DIR + "/ords_poetry_lines.csv", dtype=str).dropna()
     df = df[df["language"].isin([lang])]
-    os.system("clear")
     for n in range(verses):
-        rows = df.sample(n=lines)
+        rows = df.sample(lines)
+        for i in range(0, len(rows)):
+            print(rows.iloc[i].sentence)
+            logger.debug(rows.iloc[i].sentence)
+        print("")
+        logger.debug("")
+
+
+def test_poems():
+    langs = get_langs()
+    df = pd.read_csv(pathfuncs.DATA_DIR + "/ords_poetry_lines.csv", dtype=str).dropna()
+    for key in langs.keys():
+        print("*** {} ***".format(langs[key]))
+        rows = df[df["language"].isin([key])].sample(3)
         for i in range(0, len(rows)):
             print(rows.iloc[i].sentence)
             logger.debug(rows.iloc[i].sentence)
@@ -33,6 +43,7 @@ def dump_data(minchars=2, maxchars=32):
     logger.debug("Total translation records: {}".format(df_in.index.size))
     # Create output DataFrames, naming column `sentence` to remind that it is not the entire `problem` string.
     df_all = pd.DataFrame(columns=["sentence", "language"])
+    langs = get_langs()
     for lang in langs.keys():
         logger.debug("*** LANGUAGE {} ***".format(lang))
         # Filter for non-empty unique strings in the `problem` column.
@@ -83,9 +94,7 @@ def clean_problem(data, dedupe=True, dropna=True):
     )
     # Remove HTML symbols (&gt; features a lot).
     data.replace(
-        {"problem": r"(?i)(&[\w\s]+;)"}, {"problem": ""},
-        regex=True,
-        inplace=True
+        {"problem": r"(?i)(&[\w\s]+;)"}, {"problem": ""}, regex=True, inplace=True
     )
     # Trim whitespace from `problem` strings.
     data["problem"].str.strip()
@@ -134,6 +143,7 @@ def clean_sentence(data, dedupe=True, dropna=True):
 # Split data into lists labelled with language.
 def dump_json():
     df_in = pd.read_csv(pathfuncs.DATA_DIR + "/ords_poetry_lines.csv", dtype=str)
+    langs = get_langs()
     dict = {}
     for lang in langs.keys():
         df_tmp = df_in[df_in["language"].isin([lang])]
@@ -146,46 +156,47 @@ def dump_json():
         json.dump(dict, f, indent=4, ensure_ascii=False)
 
 
-# Get language choice.
+# Select function to run.
 def exec_opt(options):
-    for i, desc in options.items():
-        print("{} : {}".format(i, desc))
-    choice = input("Choose a language (or 0 to quit): ")
-    try:
-        choice = int(choice)
-    except ValueError:
-        print("Invalid choice")
-    else:
-        if choice == 0:
-            exit()
-        if choice >= len(options):
-            print("Out of range")
+    while True:
+        for i, desc in options.items():
+            print("{} : {}".format(i, desc))
+        choice = input("Type a number: ")
+        try:
+            choice = int(choice)
+        except ValueError:
+            print("Invalid choice")
         else:
-            lang = options[choice]
-            key = [key for key, val in langs.items() if val == lang.lower()]
-            if len(key) == 1:
-                gen(key[0])
+            if choice >= len(options):
+                print("Out of range")
             else:
-                print("Key not found: {}".format(lang))
+                f = options[choice]
+                print(f)
+                eval(f)
+
+
+def get_options():
+    return {
+        0: "exit()",
+        1: "dump_data()",
+        2: "dump_json()",
+        3: "test_poems()",
+    }
 
 
 # Map ISO lang codes to the names of the nltk language models.
-langs = {
-    "en": "english",
-    "de": "german",
-    "nl": "dutch",
-    "fr": "french",
-    "it": "italian",
-    "es": "spanish",
-    "da": "danish",
-}
+def get_langs():
+    return {
+        "en": "english",
+        "de": "german",
+        "nl": "dutch",
+        "fr": "french",
+        "it": "italian",
+        "es": "spanish",
+        "da": "danish",
+    }
 
-# Should new translations be available.
-dump_data()
-dump_json()
-exit()
 
-# Use the langs dict to create the choices.
-options = pd.Series(data=langs.values(), index=list(range(1, 8)))
-# Run command line interaction.
-exec_opt(options.str.capitalize())
+if __name__ == "__main__":
+    logger = logfuncs.init_logger(__file__)
+    exec_opt(get_options())
