4ae50c74854491f7deefaec47de6abeb955a9b77
Zenlan <1461499+zenlan@users.noreply.github.com>
2024-05-26 10:40:45 +0100

    Removes redundant lines from ordsfuncs

--- funcs/ordsfuncs.py
+++ funcs/ordsfuncs.py
@@ -1,5 +1,4 @@
 import os
-import re
 import polars as pl
 
 ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
@@ -13,40 +12,13 @@ if not os.path.exists("dat"):
     os.mkdir("dat")
 if not os.path.exists("log"):
     os.mkdir("log")
-if not os.path.exists("web"):
-    os.mkdir("web")
 if not os.path.exists("out"):
     os.mkdir("out")
-
-
-def get_envvars():
-    pathvars = {}
-    rex = re.compile(r"^PATH_TO_([_A-Z]+)")
-    for key in os.environ.keys():
-        matched = rex.search(key)
-        if matched != None:
-            pathvars[matched.group(1)] = os.environ[key]
-    return pathvars
-
-
-pathvars = get_envvars()
 
 
 def csv_path(filename):
 
     return "dat/ords/" + filename + ".csv"
-
-
-def polars_schema():
-
-    return {
-        "product_category_id": pl.Int64,
-        "year_of_manufacture": pl.Int64,
-        "product_age": pl.Float64,
-        "group_identifier": pl.String,
-        "repair_barrier_if_end_of_life": pl.String,
-        "event_date": pl.Date,
-    }
 
 
 def get_data(csvfile):
@@ -61,3 +33,13 @@ def get_data(csvfile):
     )
 
 
+def polars_schema():
+
+    return {
+        "product_category_id": pl.Int64,
+        "year_of_manufacture": pl.Int64,
+        "product_age": pl.Float64,
+        "group_identifier": pl.String,
+        "repair_barrier_if_end_of_life": pl.String,
+        "event_date": pl.Date,
+    }
