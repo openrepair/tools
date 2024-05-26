9fa7b963e60873a9c03e23a38584364bb74008f3
Zenlan <1461499+zenlan@users.noreply.github.com>
2024-05-26 17:17:04 +0100

    Refactors regex cat compiler for polars Removes redundant tmp file

--- ords_regex_category_compiler.py
+++ ords_regex_category_compiler.py
@@ -2,42 +2,40 @@
 
 
 # Given a list of "terms" (actually mini-regexes), return a whole regular expression string for each category.
-# The "terms" reside in dat/product_category_regex_elements.csv.
-# They come from a Google sheet for ease of editing and testing. Free to copy for own use.
-# https://docs.google.com/spreadsheets/d/1LVQrLXPupufRhh1aNR33R6ea3f57dcvz_QA3mumbJfQ/edit?usp=sharing
 
-from funcs import *
-import pandas as pd
+
+import polars as pl
 import re
+from funcs import *
 
 if __name__ == "__main__":
 
     logger = logfuncs.init_logger(__file__)
 
-    # Fetch regexes and test terms.
-    testterms = pd.read_csv(pathfuncs.DATA_DIR + "/ords_testdata_common_products.csv")
-    rxelems = pd.read_csv(pathfuncs.DATA_DIR + "/product_category_regex_elements.csv")
-    # Create a structure to hold compiled regexes.
-    results = pd.DataFrame(columns=["product_category", "lang", "regex"])
-    results["product_category"] = rxelems.columns
-    results["lang"] = "any"
-    results["regex"] = ""
+    testterms = pl.read_csv(ordsfuncs.DATA_DIR + "/ords_testdata_common_products.csv")
+    rxelems = pl.read_csv(ordsfuncs.DATA_DIR + "/product_category_regex_elements.csv")
 
-    # Main loop.
-    for i, row in results.iterrows():
-        category = row["product_category"]
+    regs = []
+    for category in rxelems.columns:
         print(category)
         logger.debug(category)
-        data = rxelems[category]
-        data.dropna(inplace=True)
-        regex = miscfuncs.build_regex_string(data)
+        data = rxelems.select(pl.col(category)).drop_nulls()
+        logger.debug(data[category])
+        regex = miscfuncs.build_regex_string(data[category])
+        regs.append(regex)
         rx = re.compile(regex)
-        tests = testterms.loc[testterms.product_category == category]
-        for n, test in tests.iterrows():
-            logger.debug(test["product"])
-            matches = rx.search(test["product"])
+        tests = testterms.filter(pl.col("product_category") == category)
+        for cat, prod in tests.iter_rows():
+            logger.debug(prod)
+            matches = rx.search(prod)
             logger.debug(matches)
-        results.loc[i, "regex"] = regex
-
-    # Write regex strings to csv format file.
-    results.to_csv(pathfuncs.OUT_DIR + "/product_category_regexes.csv", index=False)
+    results = pl.DataFrame(
+        data={
+            "product_category": rxelems.columns,
+            "lang": ["any"] * len(rxelems.columns),
+            "regex": regs,
+        },
+        schema={"product_category": pl.String, "lang": pl.String, "regex": pl.String},
+    )
+    logger.debug(results)
+    results.write_csv(ordsfuncs.OUT_DIR + "/product_category_regexes.csv")
